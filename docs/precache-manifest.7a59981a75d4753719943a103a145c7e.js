da28286d9e22ed687793d366c7615a2e497b6f9f
centrocomputoSTD <centrocomputoSTD@gmail.com>
2020-07-23 16:12:26 -0500

    docs agregado

---
+++ docs/precache-manifest.7a59981a75d4753719943a103a145c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43d6d881828fbf361f4ab53c337f09c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad1c5a1c8ebbfd430222",
+    "url": "/static/css/main.4696d602.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1c5a1c8ebbfd430222",
+    "url": "/static/js/main.35aaf7aa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
